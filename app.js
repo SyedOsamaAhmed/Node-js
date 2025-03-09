752cbf5c94a72306f2a3acdcf38f6da37175cd74
Syed Osama Ahmed <batch17.026@gmail.com>
2025-03-09 13:55:00 +0500

    covered streams in node js

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
-// built-in modules
+var http = require("http");
+var fs = require("fs");
 
-const { readFile, writeFile } = require("fs");
-
-readFile("./content/first.txt", "utf-8", (err, res) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  const first = res;
-  readFile("./content/second.txt", "utf-8", (err, res) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    const second = res;
-    writeFile(
-      "./content/result-async.txt",
-      `Here is the result of asynchronus reading: ${first}, ${second} `,
-      () => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-      }
-    );
+const server = http.createServer((req, res) => {
+  const fileStream = fs.createReadStream("./content/big_file.txt");
+  fileStream.on("open", () => {
+    /**
+     * streams are useful when handling big files, network communications or end to end exchange information efficientlu
+     * chunks of data processed without keeping it to memory
+     * Memory and time efficient
+     * Four types: 1) Readable 2) Writeable 3) duplex(both readable and writeable) 4) Transform (transform or modify data as it is read or wrtten)
+     * attach writable stream to readable stream where data flow is automatically managed
+     */
+    fileStream.pipe(res);
   });
+
+  fileStream.on("error", (err) => {
+    res.end(err);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
